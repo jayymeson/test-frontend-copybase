08cb3cce7adb837b6c0938b5c55655346df4c251
Jaymeson Mendes <jaymesonmendes@gmail.com>
2024-01-11 19:55:18 -0300

    fix: favicon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Copybase'; 
+        return args;
+      });
+  }
+}
